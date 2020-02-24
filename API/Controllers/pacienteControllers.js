489bfbe74f4bc96df649eedb1f2b88b05aa45cde
Bryan Silva <brandonb2816@gmail.com>
2020-02-24 18:25:40 -0300

    Solucionando conexion a Mongo

--- API/Controllers/pacienteControllers.js
+++ API/Controllers/pacienteControllers.js
@@ -2,15 +2,16 @@ const Paciente = require('../Models/Paciente');
 
 // Cuando se crea un nuevo cliente
 exports.nuevoCliente = async (req, res, next) => {
-    
+
     // Crear objeto de paciente con datos de req.body
     const paciente = new Paciente(req.body);
 
     try {
         await paciente.save();
+        res.json({ mensaje : 'El cliente se agrego correctamente'});
     } catch (error) {
         console.log(error);
         next();
     }
-    
+   
 }
