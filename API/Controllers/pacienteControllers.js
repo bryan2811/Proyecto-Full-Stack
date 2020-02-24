d6fc00c1aff59c85ebb8a847b8233c9c3cab5767
Bryan Silva <brandonb2816@gmail.com>
2020-02-24 18:37:07 -0300

    Obteniendo un registro por ID

--- API/Controllers/pacienteControllers.js
+++ API/Controllers/pacienteControllers.js
@@ -27,3 +27,14 @@ exports.obtenerPacientes = async (req, res, next) => {
         next();
     }
 }
+
+// Obtiene un paciente especifico por ID
+exports.obtenerPaciente = async (req, res, next) => {
+    try {
+        const paciente = await Paciente.findById(req.params.id);
+        res.json(paciente);
+    } catch (error) {
+        console.log(error);
+        next();
+    }
+}
